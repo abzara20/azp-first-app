21d6f32343f330a05f62a74da0e9dc569e9d913d
abzara20 <azaragozapalos@gmail.com>
2021-08-25 16:58:01 -0400

    fixing my 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Link} from 'gatsby';
 
 const Page404 = () => {
-    <React.Fragment>
-        <h1>404 Page not found</h1>
-        <Link to= '/'>Return Home</Link> 
-    </React.Fragment>
+    return(
+        <React.Fragment>
+            <h1>404 Page not found</h1>
+            <Link to= '/'>Return Home</Link> 
+        </React.Fragment>
+    )
 }
 
 export default Page404;
