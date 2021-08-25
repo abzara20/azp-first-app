f18cf160313f220463108acc37ead063bf7173a9
abzara20 <azaragozapalos@gmail.com>
2021-08-25 16:51:27 -0400

    Uploading my first app

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return(
+        <React.Fragment>
+            <h1>About Page</h1>
+        </React.Fragment>
+    );
+};
+
+export default AboutPage;
+
+//importing react, creating a variable on the page, making that variable an arrow function and exporting the variable are all the standard 
+// for creating a new page
